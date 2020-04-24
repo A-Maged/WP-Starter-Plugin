6750e7553ad6152226246ba4a36d8d145c385053
A-Maged <26769761+A-Maged@users.noreply.github.com>
2020-04-24 17:05:29 +0200

    .

--- index.php
+++ index.php
@@ -9,10 +9,6 @@ defined('ABSPATH') or die();
 
 /* load plugin last */
 add_action('plugins_loaded', function () {
-
-    /* check if "some class" you depend on is active */
-    // if (!class_exists('OxyEl'))  return;
-
     /* load plugin */
     require_once __DIR__ . '/plugin.php';
 });
