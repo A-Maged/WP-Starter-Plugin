76c970bba32f6f53fa3b998ccac67dcdc0cfd726
A-Maged <26769761+A-Maged@users.noreply.github.com>
2020-05-08 07:08:41 +0200

    SENDGRID_PASSWORD

--- plugin.php
+++ plugin.php
@@ -2,6 +2,7 @@
 
 define('BASE_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('BASE_PLUGIN_PATH', plugin_dir_path(__FILE__));
+// define('SENDGRID_PASSWORD', '');
 
 /* Autoload */
 require_once __DIR__ . '/vendor/autoload.php';
